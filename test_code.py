b5339a121d66869cbc315459d8130d6145c8b849
Юрий Силенок <yuri.silenok@gmail.com>
2020-05-24 16:01:57 +0300

    перенес тестирование на pytest

--- test_code.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import code
-
-class UT_Sum(unittest.TestCase):
-    def test_sum(self):
-        self.assertEqual(5, code.sum(2,3))
-
-if __name__ == '__main__':
-    unittest.main()
---
+++ test_pytest.py
@@ -0,0 +1,4 @@
+import my_code
+
+def test_sum():
+    assert 5 == my_code.sum(2,3)
